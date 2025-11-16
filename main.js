89f650c5f1fa7e5901ec571415362181b68f8d94
wwoozzii <gmar4630@gmail.com>
2025-11-17 00:36:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const bergerCont = document.querySelector('.burgerCont')
+const burgerCont = document.querySelector('.burgerCont')
 const burgerMenu = document.querySelector('.burgerMenu')
 const dropMenu = document.querySelector('.dropMenu')
 const ACTIVE_CLASS = 'active';
 const VISIBLE_CLASS = 'visible';
 
-burgerMenu.addEventListener("click", toggleMenu)
+burgerCont.addEventListener("click", toggleMenu)
 
 function toggleMenu() {
 	dropMenu.classList.toggle("active")
 }
 
+document.addEventListener('click', function (event) {
+	if (!burgerCont.contains(event.target)) {
+		dropMenu.classList.remove('active');
+	}
+})
